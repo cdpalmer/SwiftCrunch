33d5a0af1bcd2f6c0cefd084ecc119b306af81b4
Cody Palmer <cody.palmer08@gmail.com>
2016-03-13 21:57:58 -0600

    Stole code from solution repo

--- SwiftCrunch/Extensions.swift
+++ SwiftCrunch/Extensions.swift
@@ -9,15 +9,28 @@
 import Foundation
 
 extension Dictionary {
+    
+    // Loads a JSON file from the app bundle into a new dictionary
     static func loadJSONFromBundle(filename: String) -> Dictionary<String, AnyObject>? {
         if let path = NSBundle.mainBundle().pathForResource(filename, ofType: "json") {
             
             var error: NSError?
-            let data = NSData(contentsOfFile: path, options: NSDataReadingOptions(), error: &error)
+            let data: NSData?
+            do {
+                data = try NSData(contentsOfFile: path, options: NSDataReadingOptions())
+            } catch let error1 as NSError {
+                error = error1
+                data = nil
+            }
             if let data = data {
                 
-                let dictionary: AnyObject? = NSJSONSerialization.JSONObjectWithData(data,
-                    options: NSJSONReadingOptions(), error: &error)
+                let dictionary: AnyObject?
+                do {
+                    dictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions())
+                } catch let error1 as NSError {
+                    error = error1
+                    dictionary = nil
+                }
                 if let dictionary = dictionary as? Dictionary<String, AnyObject> {
                     return dictionary
                 } else {
